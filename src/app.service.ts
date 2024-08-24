c3c0519c2256422611177b73a00c9f646717a98b
Daly <41022166+daly64@users.noreply.github.com>
2024-08-24 20:47:32 +0100

    add vercel config cors lending page

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,28 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `
+    <style>
+      h1, h2 {
+        text-align: center;
+      }
+
+      a {
+        color: #66d9ef;
+        text-decoration: none;
+      }
+
+      a:hover {
+        text-decoration: underline;
+      }
+    </style>
+    <h1>Welcome to Todo GraphQL API</h1>
+    <h2>
+      Explore the API via
+      <a href="/graphql">GraphQL Playground</a>
+      or
+      <a href="/graphql/docs">API Documentation</a>
+    </h2>
+    `;
   }
 }

c687cf96850e096de675e49044acde3784b5cbf3
Daly <41022166+daly64@users.noreply.github.com>
2024-08-25 13:36:56 +0100

    oo

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger: ['error'] });
 
    app.enableCors();
-app.use(json);
+app.use(json({ limit: '50mb' }));
   await app.listen(3000);
 }
 bootstrap();

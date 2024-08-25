b62e228a4b279062b8940cbb7de1d0fe09f12018
Daly <41022166+daly64@users.noreply.github.com>
2024-08-25 13:02:55 +0100

    aa

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { join } from 'path';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: true,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      autoSchemaFile: true,
       // autoSchemaFile: 'schema.gql', // <-- GraphQL schema generated from schema.gql',
       installSubscriptionHandlers: true, // Enables subscriptions
     }),

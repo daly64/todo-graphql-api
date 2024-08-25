edae3288b6f900c97d2ae25b852976f9053f55b9
Daly <41022166+daly64@users.noreply.github.com>
2024-08-25 10:51:22 +0100

    tt

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn'],
+    cors: true,
+  });
 
-  // Activer CORS avec des options par défaut
-  app.enableCors();
+  
 
   await app.listen(3000);
 }

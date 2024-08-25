1702cd8f2b06bf41e6fcfb644876ca75189316c1
Daly <41022166+daly64@users.noreply.github.com>
2024-08-25 13:35:08 +0100

    o

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger: ['error'] });
 
    app.enableCors();
-
+app.use(json);
   await app.listen(3000);
 }
 bootstrap();

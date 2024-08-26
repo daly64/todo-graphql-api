1d3822922bb9c5153779f4af829959af83a813c2
Daly <41022166+daly64@users.noreply.github.com>
2024-08-26 12:38:21 +0100

    cc

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-const options = {
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  preflightContinue: true,
-  optionsSuccessStatus: 204,
-  credentials: true,
-};
 
-   app.enableCors(options);
+   app.enableCors();
 
   await app.listen(3000);
 }

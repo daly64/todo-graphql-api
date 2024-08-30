93420e8d4fc55859a1b4016041dff1fd18640f51
Daly <41022166+daly64@users.noreply.github.com>
2024-08-30 12:34:57 +0100

    ssa

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
